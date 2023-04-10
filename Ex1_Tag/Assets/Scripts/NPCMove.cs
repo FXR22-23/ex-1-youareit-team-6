8d108427fcc7fb7dfe430ec2c2f3d82cf14497cc
TheLion10 <itzhak.galperin@mail.huji.ac.il>
2023-04-10 15:33:04 +0300

    finished game

--- Ex1_Tag/Assets/Scripts/NPCMove.cs
+++ Ex1_Tag/Assets/Scripts/NPCMove.cs
@@ -1,18 +1,22 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEditor.VersionControl;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class NPCMove : MonoBehaviour
 {
     public Transform goal;
-    private float speed = 0.001f;
+    private float speed = 0.005f;
     private float rotationSpeed = 0.03f;
     private float distance = 0.5f;
     private bool stayIdle = true;
     private bool isChaser = true;
     private bool canBeCaught = false;
+
+    public Transform runawayDestination;
 
     private void Start()
     {
@@ -27,7 +31,7 @@ public class NPCMove : MonoBehaviour
         if (!GetComponent<Animator>().GetBool("Is Walking") && !GetComponent<Animator>().GetBool("Is Running"))
         {
             GetComponent<Animator>().SetBool("Is Walking", true);
-            speed = 0.001f;
+            speed = 0.005f;
             Invoke("StartRunning", 2);
         }
 
@@ -52,8 +56,9 @@ public class NPCMove : MonoBehaviour
         
         if (direction.magnitude >= distance)
         {
-            Vector3 pushVector = direction.normalized * speed;
-            transform.Translate(pushVector, Space.World);
+            // Vector3 pushVector = direction.normalized * speed;
+            // transform.Translate(pushVector, Space.World);
+            GetComponent<NavMeshAgent>().SetDestination(goal.position);
         }
         else // caught the player
         {
@@ -67,15 +72,20 @@ public class NPCMove : MonoBehaviour
 
     void RunAway()
     {
-        Vector3 realGoal = new Vector3(goal.position.x, 
-            transform.position.y, goal.position.z);
-        Vector3 direction = transform.position - realGoal;
-
+        Vector3 realGoal = new Vector3(runawayDestination.position.x, 
+            transform.position.y, runawayDestination.position.z);
+        Vector3 direction = realGoal - transform.position;
+        
         transform.rotation = Quaternion.Slerp(transform.rotation, 
             Quaternion.LookRotation(direction), rotationSpeed);
         
-        Vector3 pushVector = direction.normalized * speed;
-        transform.Translate(pushVector, Space.World);
+        realGoal = new Vector3(goal.position.x, 
+            transform.position.y, goal.position.z);
+        direction = transform.position - realGoal;
+        // Vector3 pushVector = direction.normalized * speed;
+        // transform.Translate(pushVector, Space.World);
+
+        GetComponent<NavMeshAgent>().SetDestination(runawayDestination.position);
         
         if (canBeCaught && direction.magnitude <= distance)
         {
@@ -96,7 +106,7 @@ public class NPCMove : MonoBehaviour
     {
         GetComponent<Animator>().SetBool("Is Running", true);
         GetComponent<Animator>().SetBool("Is Walking", false);
-        speed = 0.004f;
+        speed = 0.01f;
     }
 
     void CanBeCaught()
